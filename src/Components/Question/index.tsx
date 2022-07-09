acc3cd71806b146968fc666c3cdc473dbcf35e64
Alpay Oğuzhan Palabıyık <alpayoguz97@gmail.com>
2022-07-09 12:05:24 +0300

    add style changes

--- src/Components/Question/index.tsx
+++ src/Components/Question/index.tsx
@@ -10,10 +10,10 @@ const Question:React.FC = () => {
             <p className=''>What is the founding date of republic ?</p>
         </div>
         <div className='question__choices'>
-            <div className='question__choice'>A: ankara</div>
-            <div className='question__choice'>B: ankara</div>
-            <div className='question__choice'>C: ankara</div>
-            <div className='question__choice'>D: ankara</div>
+            <div className='question__choice'><span>A:</span> ankara</div>
+            <div className='question__choice'><span>B:</span> ankara</div>
+            <div className='question__choice'><span>C:</span> ankara</div>
+            <div className='question__choice'><span>D:</span> ankara</div>
 
         </div>
 
